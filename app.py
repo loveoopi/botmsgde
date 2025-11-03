ff3b4f15e7a0d17973e9c9495fe63d8942f168cf
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-11-04 01:37:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Configuration
-DELETE_DELAY = 5  # 5 seconds
+DELETE_DELAY = 200  # 200 seconds
 
 class TelegramMessageDeleter:
     def __init__(self):
